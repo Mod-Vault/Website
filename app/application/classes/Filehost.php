769c6e8df6210c6748195f649c8e1510b6f35326
GrumpyCrouton <12193403+GrumpyCrouton@users.noreply.github.com>
2025-03-27 23:15:10 -0500

    Finish implementing Game Manager

--- app/application/classes/Filehost.php
+++ app/application/classes/Filehost.php
@@ -55,4 +55,25 @@ class Filehost extends \Model {
 
     }
 
+    public function UploadImgBB($image, $name = null) {
+
+        $f3 = Base::instance();
+        if(empty($f3->imgbb_api_key)) return "No API Key";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.imgbb.com/1/upload?key=' . $f3->imgbb_api_key);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+        $file_name = ($name) ? $name . '.' . $extension : $image['name'];
+        $data = array('image' => base64_encode(file_get_contents($image['tmp_name'])), 'name' => $file_name);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return 'Error:' . curl_error($ch);
+        } else {
+            return json_decode($result, true);
+        }
+        curl_close($ch);
+    }
 }
--- app/application/classes/Game.php
+++ app/application/classes/Game.php
@@ -22,8 +22,53 @@ class Game extends DBEntity {
         return json_decode(json_encode($all_mods));
     }
 
+    function Update($data) {
+        if(!$this->IsValidEntity()) return;
+
+        $values = $this->CleanData($data, ['name', 'description', 'link']);
+
+        if(array_key_exists('thumbnail', $_FILES) && $_FILES['thumbnail']['size'] > 0) {
+            $fh = new Filehost();
+
+            echo "<pre>" . print_r($_FILES,true) . "</pre>";
+
+            $image_url = $fh->UploadImgBB($_FILES['thumbnail']);
+            if(!is_array($image_url) || !$image_url['success']) {
+                die("<pre>" . print_r(['problem uploading image', $image_url],true) . "</pre>");
+            }
+
+            $values['thumbnail'] = $image_url['data']['url'];
+        }
+
+        $this->db->update('games', $values, ['uid' => $this->uid]);
+    }
+
     function Create($data) {
         if($this->IsValidEntity()) return;
+
+        $values = $this->CleanData($data, ['name', 'description', 'link']);
+
+        $game_exists = $this->db->column('SELECT uid FROM games WHERE name=?', [$values['name']]);
+
+        if(!empty($game_exists)) {
+            return "A game with that name already exists (UID: {$game_exists})";
+        }
+
+        if(array_key_exists('thumbnail', $_FILES) && $_FILES['thumbnail']['size'] > 0) {
+            $fh = new Filehost();
+
+            $image_url = $fh->UploadImgBB($_FILES['thumbnail']);
+            if(!is_array($image_url) || !$image_url['success']) {
+                die("<pre>" . print_r(['problem uploading image', $image_url],true) . "</pre>");
+            }
+
+            $values['thumbnail'] = $image_url['data']['url'];
+        }
+
+        $this->db->insert('games', $values);
+        $this->uid = $this->db->lastInsertId();
+
+        $this->GetData();
     }
 
     function PullData() {
@@ -32,11 +77,22 @@ class Game extends DBEntity {
             SELECT
                 uid,
                 thumbnail,
-                name
+                name,
+                link,
+                description
             FROM
                 games
             WHERE
                 uid=?
         ', [$this->uid]);
+    }
+
+    function GetGames() {
+        return (object)$this->db->all('
+            SELECT
+                *
+            FROM
+                games
+        ');
     }
 }
