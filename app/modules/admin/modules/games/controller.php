12d8e1ec331c61dc8143b4b21de37f0b8bfdc96e
GrumpyCrouton <12193403+GrumpyCrouton@users.noreply.github.com>
2025-03-27 22:05:45 -0500

    Update Edit/Create Game page

--- app/modules/admin/modules/games/controller.php
+++ app/modules/admin/modules/games/controller.php
@@ -4,7 +4,12 @@ class controller extends \AdminController {
 	function get() {
 		echo $this->render('index');
 	}
-	function edit() {
+	function edit($f3) {
+
+		$game = !empty($params['resource_id']) ? new \Game($params['resource_id'], true) : new \Game();
+
+		$f3->set('game', $game);
+
 		echo $this->render('edit');
 	}
 }
